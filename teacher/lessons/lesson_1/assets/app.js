ed66b3cdab14385c978364495ad861dc5f669b4d
Farhod Allamuradov <farkhod5allamuradov@gmail.com>
2021-07-13 08:09:04 +0500

    Add lesson 2

---
+++ teacher/lessons/lesson_1/assets/app.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML = "This is updated text!";
+
+// CRUD => Create, Read, Update,  Delete
+
+// alert("This is alert message!");
+
+console.log(document);
---
+++ teacher/lessons/lesson_2/assets/app.js
@@ -0,0 +1,36 @@
+let x = 24;
+x += 5; 
+let y = 45;
+let z = x * y;  // Expression - Algebraik ifoda
+let a = 5 ** 2;
+
+console.log(a);
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1 + ' ' + text2;
+
+console.log(text3);
+
+let text4 = "What a very ";
+text4 += "nice day";
+
+console.log(text4);
+
+console.log(x < y);
+
+let c = "35";
+let d = 35;
+
+console.log(c == d);
+console.log(c === d);
+
+console.log(c == d && c === d);
+console.log(!(c == d));
+
+console.log(typeof c);
+
+let e = ++d + 4;
+
+console.log(e);
+console.log(d);
